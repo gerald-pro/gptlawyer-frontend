645fcd881e6bd3e2d5713265905d8804d8cd527e
Gerald Avalos <avaloss.gerald@gmail.com>
2024-06-25 02:32:15 -0400

    .env

--- src/apollo-config.ts
+++ src/apollo-config.ts
@@ -4,7 +4,10 @@ import { setContext } from '@apollo/client/link/context';
 import createUploadLink from 'apollo-upload-client/createUploadLink.mjs';
 import { useUserStore } from './stores/user';
 
-const httpLink = createUploadLink({ uri: "http://127.0.0.1:8000/graphql" });
+
+const backendUrl = import.meta.env.VITE_APP_BACKEND_URL || "http://127.0.0.1:8000/graphql";
+
+const httpLink = createUploadLink({ uri: backendUrl });
 
 const authLink = setContext((_, { headers }) => {
   const userStore = useUserStore();
